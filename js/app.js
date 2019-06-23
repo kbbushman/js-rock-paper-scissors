4a4cd5442eb77228d9f0a33f190edae68e578ace
kbbushman <k.bushman@yahoo.com>
2019-06-23 16:34:52 -0700

    Adds game logic

--- js/app.js
+++ js/app.js
@@ -1,1 +1,116 @@
-console.log('Hello Dave...');
+const choices = document.querySelectorAll('.choice');
+const score = document.getElementById('score');
+const result = document.getElementById('result');
+const restart = document.getElementById('restart');
+const modal = document.querySelector('.modal');
+const scoreboard = {
+  player: 0,
+  computer: 0,
+};
+
+// Play Game
+function play(e) {
+  restart.style.display = 'inline-block';
+  const playerChoice = e.target.id;
+  const computerChoice = getComputerChoice();
+  const winner = getWinner(playerChoice, computerChoice);
+  showWinner(winner, computerChoice);
+};
+
+// Computer Choice
+function getComputerChoice() {
+  const random = Math.random();
+  if (random < 0.34) {
+    return 'rock';
+  } else if (random <= 0.67) {
+    return 'paper';
+  } else {
+    return 'scissors';
+  }
+};
+
+// Get Winner
+function getWinner(p, c) {
+  if (p === c) {
+    return 'draw';
+  } else if (p === 'rock') {
+    if (c === 'paper') {
+      return 'computer';
+    } else {
+      return 'player';
+    }
+  } else if (p === 'paper') {
+    if (c === 'scissors') {
+      return 'computer';
+    } else {
+      return 'player';
+    }
+  } else if (p === 'scissors') {
+    if (c === 'rock') {
+      return 'computer';
+    } else {
+      return 'player';
+    }
+  }
+};
+
+// Show Winner
+function showWinner(winner, computerChoice) {
+  if (winner === 'player') {
+    // Increment player score
+    scoreboard.player++;
+    // Show modal
+    result.innerHTML = `
+      <h1 class="text-win">You Win</h1>
+      <i class="fas fa-hand-${computerChoice} fa-10x"></i>
+      <p>Computer Chose <strong>${computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)}</strong></p>
+    `;
+  } else if (winner === 'computer') {
+    // Increment player score
+    scoreboard.computer++;
+    // Show modal
+    result.innerHTML = `
+      <h1 class="text-lose">You Lose</h1>
+      <i class="fas fa-hand-${computerChoice} fa-10x"></i>
+      <p>Computer Chose <strong>${computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)}</strong></p>
+    `;
+  } else {
+    // Show modal
+    result.innerHTML = `
+      <h1>It's A Draw!</h1>
+      <i class="fas fa-hand-${computerChoice} fa-10x"></i>
+      <p>Computer Chose <strong>${computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)}</strong></p>
+    `;
+  }
+
+  // Show Score
+  score.innerHTML = `
+    <p>Player: ${scoreboard.player}</p>
+    <p>Computer: ${scoreboard.computer}</p>
+  `;
+
+  // Show Modal
+  modal.style.display = 'block';
+};
+
+// Clear Modal
+function clearModal(e) {
+  if (e.target === modal) {
+    modal.style.display = 'none';
+  }
+};
+
+// Restart Game
+function restartGame() {
+  scoreboard.player = 0;
+  scoreboard.computer = 0;
+  score.innerHTML = `
+    <p>Player: 0</p>
+    <p>Computer: 0</p>
+  `;
+}
+
+// Event Listeners
+choices.forEach(choice => choice.addEventListener('click', play));
+window.addEventListener('click', clearModal);
+restart.addEventListener('click', restartGame);
